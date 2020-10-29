eac51a9aa2320a636aa4c0942a8719055839be70
Alex2055 <alexfromnashville@gmail.com>
2020-10-29 17:27:16 -0500

    22.3

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { idbPromise } from "../../utils/helpers";
 import { useStoreContext } from '../../utils/GlobalState';
 import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
+
 
 const CartItem = ({ item }) => {
 
@@ -11,6 +13,7 @@ const CartItem = ({ item }) => {
       type: REMOVE_FROM_CART,
       _id: item._id
     });
+    idbPromise('cart', 'delete', { ...item });
   };
   const onChange = (e) => {
     const value = e.target.value;
@@ -20,15 +23,18 @@ const CartItem = ({ item }) => {
         type: REMOVE_FROM_CART,
         _id: item._id
       });
+    
+      idbPromise('cart', 'delete', { ...item });
     } else {
       dispatch({
         type: UPDATE_CART_QUANTITY,
         _id: item._id,
         purchaseQuantity: parseInt(value)
       });
+    
+      idbPromise('cart', 'put', { ...item, purchaseQuantity: parseInt(value) });
     }
-  };
-
+  }
   return (
     <div className="flex-row">
       <div>
